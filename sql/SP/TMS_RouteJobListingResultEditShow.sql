c0e79800ad3261bc555d15b18e00261d5d78c476
ranggad990@gmail.com <ranggad990@gmail.com>
2018-06-22 16:44:12 +0700

    sp

--- sql/SP/TMS_RouteJobListingResultEditShow.sql
+++ sql/SP/TMS_RouteJobListingResultEditShow.sql
@@ -6,22 +6,140 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 ALTER PROCEDURE [dbo].[TMS_RouteJobListingResultEditShow] --exec [dbo].[TMS_RouteJobListingResultEditShow] '20180621_154747795'
-@RunId varchar(100)
+@RunId varchar(100), @OriRunId VARCHAR(100)
 AS
 SET NOCOUNT ON;
---DECLARE @RunId varchar(100) = '20180621_154747795';
-DECLARE @sap AS TABLE
-	(
-		Cust VARCHAR(100) NULL,
-		DOPR VARCHAR(100) NULL,
-		DOSP VARCHAR(100) NULL,
-		DOSS VARCHAR(100) NULL,
-		DORS VARCHAR(100) NULL
-	);
+--DECLARE @RunId VARCHAR(100)= '20180621_154747795';
+
+IF(
+	SELECT
+		COUNT(*)
+	FROM
+		bosnet1.dbo.TMS_RouteJob
+	WHERE
+		RunID = @RunId
+)= 0 BEGIN --PRINT 'Hi Ravi Anand';
+
+--bosnet1.dbo.TMS_RouteJob
+ INSERT
+	INTO
+		bosnet1.dbo.TMS_RouteJob SELECT
+			job_id,
+			customer_id,
+			do_number,
+			vehicle_code,
+			activity,
+			routeNb,
+			jobNb,
+			arrive,
+			depart,
+			@RunId,
+			getdate(),
+			branch,
+			shift,
+			lon,
+			lat,
+			weight,
+			volume,
+			transportCost,
+			activityCost,
+			Dist,
+			isFix
+		FROM
+			BOSNET1.dbo.TMS_RouteJob
+		WHERE
+			RunId = @OriRunId;
+
+--bosnet1.dbo.TMS_PreRouteJob
+ INSERT
+	INTO
+		bosnet1.dbo.TMS_PreRouteJob SELECT
+			@RunId,
+			Customer_ID,
+			DO_Number,
+			Long,
+			Lat,
+			Customer_priority,
+			Service_time,
+			deliv_start,
+			deliv_end,
+			vehicle_type_list,
+			total_kg,
+			total_cubication,
+			DeliveryDeadline,
+			DayWinStart,
+			DayWinEnd,
+			format(getdate(),'yyyy-MM-dd hh:mm'),
+			format(getdate(),'yyyy-MM-dd hh:mm'),
+			isActive,
+			Is_Exclude,
+			Is_Edit,
+			Product_Description,
+			Gross_Amount,
+			DOQty,
+			DOQtyUOM,
+			Name1,
+			Street,
+			Distribution_Channel,
+			Customer_Order_Block_all,
+			Customer_Order_Block,
+			Request_Delivery_Date,
+			MarketId,
+			Desa_Kelurahan,
+			Kecamatan,
+			Kodya_Kabupaten,
+			Batch,
+			Ket_DO
+		FROM
+			bosnet1.dbo.TMS_PreRouteJob
+		WHERE
+			RunId = @OriRunId;
+
+--BOSNET1.dbo.TMS_PreRouteVehicle
+ INSERT
+	INTO
+		bosnet1.dbo.TMS_PreRouteVehicle SELECT
+			@RunId,
+			vehicle_code,
+			weight,
+			volume,
+			vehicle_type,
+			branch,
+			startLon,
+			startLat,
+			endLon,
+			endLat,
+			startTime,
+			endTime,
+			source1,
+			format(getdate(),'yyyy-MM-dd'),
+			format(getdate(),'yyyy-MM-dd'),
+			isActive,
+			fixedCost,
+			costPerM,
+			costPerServiceMin,
+			costPerTravelMin,
+			IdDriver,
+			NamaDriver,
+			agent_priority,
+			max_cust
+		FROM
+			bosnet1.dbo.TMS_PreRouteVehicle
+		WHERE
+			RunId = @OriRunId;
+END DECLARE @sap AS TABLE
+(
+	Cust VARCHAR(100) NULL,
+	DOPR VARCHAR(100) NULL,
+	DOSP VARCHAR(100) NULL,
+	DOSS VARCHAR(100) NULL,
+	DORS VARCHAR(100) NULL
+);
 
 INSERT
 	INTO
-		@sap EXEC [dbo].[TMS_CekDataShipmentSAP] @RunId;
+		@sap EXEC dbo.TMS_CekDataShipmentSAP @RunId;
+
 SELECT
 	CASE
 		WHEN rj.depart = '' THEN 0
@@ -146,4 +264,4 @@ WHERE
 	rj.runID = @RunId
 ORDER BY
 	rj.routeNb,
-	rj.jobNb
+	rj.jobNb;
